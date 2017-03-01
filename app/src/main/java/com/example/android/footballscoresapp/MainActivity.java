425caf51f766e560d08729eebf4698a9edc3404d
norahalsaaran <noura_alsaaran@hotmail.com>
2017-03-02 00:56:09 +0300

    update

--- app/src/main/java/com/example/android/footballscoresapp/MainActivity.java
+++ app/src/main/java/com/example/android/footballscoresapp/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     int team2_score = 0 ;
     int team2_faul = 0 ;
 
+    //------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
