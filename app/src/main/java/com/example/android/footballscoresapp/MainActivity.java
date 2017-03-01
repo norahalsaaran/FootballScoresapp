38c87ae1cbfcb2643b6fa41da43240f3999a4ba0
norahalsaaran <noura_alsaaran@hotmail.com>
2017-03-02 00:54:45 +0300

    update

--- app/src/main/java/com/example/android/footballscoresapp/MainActivity.java
+++ app/src/main/java/com/example/android/footballscoresapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 
